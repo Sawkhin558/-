8ba13cb8133cd4b4add8c3d56255f459484f9b27
Sawkhin558 <245351026+Sawkhin558@users.noreply.github.com>
2026-05-11 05:04:36 -0400

    feat: Add Capacitor and Android support

---
+++ android/app/src/main/java/com/myanmar/exchange/pos/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myanmar.exchange.pos;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myanmar.exchange.pos',
+  appName: 'Myanmar Exchange',
+  webDir: 'dist'
+};
+
+export default config;
